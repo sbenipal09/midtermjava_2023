a168a3b2bb75efea6ae89e56e19fd68da4f09017
Simranjeet Singh <sin12743@sheridancollege.ca>
2023-06-19 08:43:09 -0400

    created input html file

--- src/main/java/sheridan/sin12743/processors/controller/ProcessorsController.java
+++ src/main/java/sheridan/sin12743/processors/controller/ProcessorsController.java
@@ -1,4 +1,21 @@
 package sheridan.sin12743.processors.controller;
 
+import org.springframework.ui.Model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ProcessorsController {
+    private final Logger log = LoggerFactory.getLogger(ProcessorsController.class);
+ @GetMapping("/"){
+     public String input(){
+         log.trace("input() called");
+    return "input";
+     }
+ }
 }
